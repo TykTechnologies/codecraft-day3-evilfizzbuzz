5770b55e300415d875009dc6b8cfb995931ec896
James Lucktaylor <jlucktay+github@gmail.com>
2022-05-18 11:06:39 +0100

    feat: orchestrate main from one to three

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/TykTechnologies/codecraft-day3-evilfizzbuzz/one"
+import (
+	"github.com/TykTechnologies/codecraft-day3-evilfizzbuzz/one"
+	"github.com/TykTechnologies/codecraft-day3-evilfizzbuzz/three"
+	"github.com/TykTechnologies/codecraft-day3-evilfizzbuzz/two"
+)
 
 func main() {
-	one.GenerateInput(1, 100)
+	fromOne := one.GenerateInput(1, 100)
+
+	fromTwo := two.ReplaceDivisibleByThree(fromOne)
+
+	br := three.NewBuzzReplacer()
+	sendToFour := br.ReplaceListOfStringsWithBuzz(fromTwo)
+
+	_ = sendToFour // TODO
 }
