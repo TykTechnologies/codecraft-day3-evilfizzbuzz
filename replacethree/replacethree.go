b35ab79f29c04cba6f6def410ccb1c91b41df446
Kofo Okesola <okesolakofo@gmail.com>
2022-05-18 10:53:25 +0100

    feat: divisible by three replacement done

---
+++ replacethree/replacethree.go
@@ -0,0 +1,15 @@
+package replacethree
+
+import "fmt"
+
+func ReplaceDivisibleByThree(input []int) []string {
+	out := make([]string, len(input))
+	for i, val := range input {
+		if val%3 == 0 && val%5 != 0 {
+			out[i] = "Fizz"
+			continue
+		}
+		out[i] = fmt.Sprintf("%d", val)
+	}
+	return out
+}
--- replacethree/replacethree_test.go
+++ replacethree/replacethree_test.go
@@ -1,6 +1,19 @@
 package replacethree
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestReplaceDivisibleByThreeInSequence(t *testing.T) {
+	input := []int{1, 3, 6, 9, 19}
+	expected := []string{"1", "Fizz", "Fizz", "Fizz", "19"}
+	assert.Equal(t, expected, ReplaceDivisibleByThree(input))
+}
+
+func TestDoesntReplaceDivisibleByFiveInSequence(t *testing.T) {
+	input := []int{1, 3, 6, 15, 20, 18, 19}
+	expected := []string{"1", "Fizz", "Fizz", "15", "20", "Fizz", "19"}
+	assert.Equal(t, expected, ReplaceDivisibleByThree(input))
 }
